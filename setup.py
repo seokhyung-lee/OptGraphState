662199b6b1cc99f54958cfd5f199e90922918c8c
Seok-Hyung Lee <sh.lee1524@gmail.com>
2023-04-17 21:31:35 +1000

    Minor updates

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="optgraphstate",
+    version="0.1.0",
+    author="Seok-Hyung Lee",
+    author_email="sh.lee1524@gmail.com",
+    description="Graph-theoretical optimization of fusion-based graph state generation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seokhyung-lee/OptGraphState",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
